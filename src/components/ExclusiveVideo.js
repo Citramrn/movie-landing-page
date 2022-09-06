2d45bd2fd843f0ec812f9e2c6b711a7e5d240e22
Citra Mirna <ctrmirna@gmail.com>
2022-09-06 12:53:35 +0700

    fix : edit icon video

--- src/components/ExclusiveVideo.js
+++ src/components/ExclusiveVideo.js
@@ -1,7 +1,8 @@
 import ImgOne from '../assets/adventure.jpg'
 import ImgTwo from '../assets/animated.jpg'
 import ImgThree from '../assets/spider-man-no-way-home.jpg'
-import ImgFour from '../assets/thriller.jpg'
+import ImgFour from '../assets/sci-fi.jpg'
+import Icon from '../assets/Play.png'
 
 function Movie() {
     return (
@@ -13,21 +14,27 @@ function Movie() {
                 <div className="cast-card-video">
                     <div className="card-head-video">
                         <img src={ImgOne} alt="captai-marvel" className='card-img rounded' />
+                        <img src={Icon} alt="icon" className='center' />
                     </div>
                 </div>
                 <div className="cast-card-video">
                     <div className="card-head-video">
                         <img src={ImgTwo} alt="captai-marvel" className='card-img rounded' />
+                        <img src={Icon} alt="icon" className='center' />
+
                     </div>
                 </div>
                 <div className="cast-card-video">
                     <div className="card-head-video">
                         <img src={ImgThree} alt="captai-marvel" className='card-img rounded' />
+                        <img src={Icon} alt="icon" className='center' />
+
                     </div>
                 </div>
                 <div className="cast-card-video">
                     <div className="card-head-video">
                         <img src={ImgFour} alt="captai-marvel" className='card-img rounded' />
+                        <img src={Icon} alt="icon" className='center' />
                     </div>
                 </div>
             </div>
